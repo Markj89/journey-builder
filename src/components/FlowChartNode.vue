672d697a2d7a1d439e84b084ed136eb03f9b4fa6
Marcus Jackson <marcus@maropost.com>
2019-07-23 15:02:24 -0500

    Default Filter Node

--- src/components/FlowChartNode.vue
+++ src/components/FlowChartNode.vue
@@ -5,33 +5,7 @@
       <div class="icon" v-bind:style="{ 'background-image': 'url(' + icon + ')' }"></div>
       <h1 v-text="label" class="title node-label"></h1>
     </div>
-    <div class="node-port node-output" @mousedown="outputMouseDown" v-if="`${type}` === 'action' ||`${type}` === 'trigger' || `${type}` === 'delay'"></div>
-    <div class="node-port node-dbl-output" @mousedown="outputMouseDown" v-if="`${type}` === 'filter'"></div>
-    <svg v-if="`${label}` === 'Yes/No' || `${label}` === '% Split'" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 107.5 70.52" xml:space="preserve" class="node-dbl-output" width="120px" height="100%" :style="doubleArrow">
-      <symbol id="Arrow_3" viewBox="-11.59 -10.76 23.18 21.53">
-        <g>
-          <g>
-            <g>
-              <polygon fill="#EB2D34" points="-11.59,-10.76 -2.67,0 -11.59,10.76 11.59,0"/>
-            </g>
-          </g>
-          <g>
-            <g>
-              <polygon fill="#231F20" points="-11.59,-10.76 -2.67,0 -11.59,10.76 11.59,0"/>
-            </g>
-          </g>
-        </g>
-      </symbol>
-      <g>
-        <line fill="none" stroke="#000000" stroke-miterlimit="10" x1="0" y1="35.5" x2="41" y2="35.5"/>
-        <line fill="none" stroke="#000000" stroke-miterlimit="10" x1="40.59" y1="35.09" x2="71" y2="65.5"/>
-  	     <line fill="none" stroke="#000000" stroke-miterlimit="10" x1="40.59" y1="35.5" x2="71" y2="5.09"/>
-        <line fill="none" stroke="#000000" stroke-miterlimit="10" x1="70.5" y1="5.5" x2="100.5" y2="5.5"/>
-        <line fill="none" stroke="#000000" stroke-miterlimit="10" x1="70.5" y1="65.5" x2="100.5" y2="65.5"/>
-        <use xlink:href="#Arrow_3" width="23.18" height="21.53" x="-11.59" y="-10.76" transform="matrix(0.4745 0 0 -0.4887 102.0001 5.2609)" overflow="visible"/>
-        <use xlink:href="#Arrow_3" width="23.18" height="21.53" x="-11.59" y="-10.76" transform="matrix(0.4745 0 0 -0.4887 102.0001 65.2609)" overflow="visible"/>
-      </g>
-    </svg>
+    <div class="node-port node-output" @mousedown="outputMouseDown" v-if="`${type}` === 'action' ||`${type}` === 'trigger' || `${type}` === 'delay' || `${type}` === 'filter'"></div>
     <div v-show="show.delete" class="node-delete">&times;</div>
   </div>
 </template>
@@ -102,17 +76,6 @@ export default {
         left: this.options.centerX + this.x * this.options.scale + 'px', // remove: this.options.offsetLeft +
         transform: `scale(${this.options.scale})`,
       };
-    },
-    doubleArrow() {
-      return {
-        stroke: 'rgb(2, 2, 2)',
-        position: 'relative',
-        top: '-80px',
-        left: '80px',
-        transform: `scale(${this.options.scale})`,
-        strokeWidth: 2,
-        fill: 'none',
-      }
     }
   },
   methods: {
@@ -122,7 +85,7 @@ export default {
     },
     handleMousedown(e) {
       const target = e.target || e.srcElement;
-      if (target.className.indexOf('node-input') < 0 && target.className.indexOf('node-output') < 0 && target.className.indexOf('node-dbl-output') < 0) {
+      if (target.className.indexOf('node-input') < 0 && target.className.indexOf('node-output') < 0) {
         this.$emit('nodeSelected', e);
       }
       e.preventDefault();
