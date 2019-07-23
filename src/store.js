93ac8468a8977005b4b23b92a979ee03affa829e
Marcus Jackson <marcus@maropost.com>
2019-07-23 15:10:46 -0500

    Ternary API Url

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 import axios from 'axios'
 
 Vue.use(Vuex, axios)
-const API_URL = 'http://localhost:8080';
+const API_URL = window.URL.href  ? 'http://journeybuilder.maropost.com/' : 'http://localhost:8080/';
 
 export default new Vuex.Store({
   debug: true,
@@ -21,7 +21,7 @@ export default new Vuex.Store({
   },
   actions: {
     async getDummyData( { commit } ) {
-      axios.get(`${API_URL}/content.json`).then((response) => {
+      axios.get(`${API_URL}content.json`).then((response) => {
         commit('GET_POST', response.data);
       }).catch((error) => {
         console.log(error);
